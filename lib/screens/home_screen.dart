6b7ada9867c8df0c80bed590bad6c74ba208c4a6
Santosh Bhandari <santoshvandari1@gmail.com>
2024-11-14 15:10:30 +0545

    Created Files

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:qr_code_scanner_plus/qr_code_scanner_plus.dart';
+import 'dart:io';
+import 'result_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+
+  // Function to scan QR code from camera
+  void _scanQRCode(BuildContext context) async {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => QRView(
+          key: qrKey,
+          onQRViewCreated: (controller) {
+            controller.scannedDataStream.listen((scanData) {
+              controller.pauseCamera();
+              Navigator.pop(context); // Go back to home
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ResultScreen(data: scanData.code),
+                ),
+              );
+            });
+          },
+        ),
+      ),
+    );
+  }
+
+  // Function to pick QR code image from gallery
+  void _pickImageFromGallery(BuildContext context) async {
+    final pickedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      final file = File(pickedImage.path);
+      // Here you would implement a method to decode the QR code from the image file
+      String? decodedData =
+          await decodeQRCodeFromImage(file); // Implement this function
+      if (decodedData != null) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ResultScreen(data: decodedData),
+          ),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('QR Code Scanner')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => _scanQRCode(context),
+              child: Text('Scan QR Code from Camera'),
+            ),
+            ElevatedButton(
+              onPressed: () => _pickImageFromGallery(context),
+              child: Text('Upload QR Code from Gallery'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ResultScreen extends StatelessWidget {
-  const ResultScreen({super.key});
+  final String data;
+
+  const ResultScreen({required this.data});
+
+  void _copyToClipboard(BuildContext context) {
+    Clipboard.setData(ClipboardData(text: data));
+    Fluttertoast.showToast(
+      msg: "QR Data Copied!",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('QR Data')),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                data,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _copyToClipboard(context),
+                child: Text('Copy to Clipboard'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
